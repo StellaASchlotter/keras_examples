c42e9a1fe973c3d750615e1d509b9bba8bd5501b
Stella Alice Schlotter <stella.schlotter@deepl.com>
2023-06-30 15:33:53 +0200

    added example for custom loss for intermediate output

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from tensorflow import keras
 from tensorflow.keras.models import Model
 import numpy as np
 
+def i_am_a_cool_loss_function(y_true, y_pred):
+    return 0.42
+
 # create some training data
 train_data = np.array([[1.0,1.0]])
 train_targets = np.array([2.0])
@@ -23,16 +26,15 @@ for i in range(4,8000,4):
 
 layer_in = keras.layers.Input(shape=(2,))
 layer1 = keras.layers.Flatten()(layer_in)
-layer2 = keras.layers.Dense(20, activation=tf.nn.relu)(layer1)
+layer2 = keras.layers.Dense(20,name="my_cool_intermediate_output", activation=tf.nn.relu)(layer1)
 layer3 = keras.layers.Dense(20, activation=tf.nn.relu)(layer2)
-layer4 = keras.layers.Dense(1)(layer3)
+layer4 = keras.layers.Dense(1, name="final")(layer3)
 
 model = Model(inputs=layer_in,
-                    outputs=[layer4, layer2])
+                    outputs=[layer2, layer4])
 
 model.compile(optimizer='adam', 
-              loss='mse',
-              metrics=['mae'])
+              loss={'my_cool_intermediate_output':i_am_a_cool_loss_function, 'final': "mse"})
 
 model.fit(train_data, train_targets, epochs=1, batch_size=1)
 
